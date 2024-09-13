1e0b8af4561d3095b9a6d404926222d829c34aeb
dmtungTma <dmtung@tma.com.vn>
2024-09-13 16:00:03 +0700

    Update Interface và Response

--- app/src/main/java/com/example/dualscreen/interface/DogApi.kt
+++ app/src/main/java/com/example/dualscreen/interface/DogApi.kt
@@ -1,12 +1,8 @@
 package com.example.dualscreen.`interface`
-import com.example.dualscreen.response.DogImageResponse
 import com.example.dualscreen.response.DogImageResponses
 import retrofit2.http.GET
 
 interface DogApi {
-    @GET("breeds/image/random")
-    suspend fun getRandomDogImage(): retrofit2.Response<DogImageResponse>
-
     @GET("breeds/image/random/5")
     suspend fun getRandomDogImages(): retrofit2.Response<DogImageResponses>
 }
